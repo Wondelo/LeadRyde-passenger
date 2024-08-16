4de893747a4d21e5e8236e2773cef867c3dc7975
Mehtab Ahmed <mehtab.ahmad@ellogroup.co.uk>
2024-08-16 13:22:45 +0100

    New code received with staging URL (demo.leadryde.com) enabled

--- app/src/main/java/com/utils/CommonUtilities.java
+++ app/src/main/java/com/utils/CommonUtilities.java
@@ -8,7 +8,8 @@ public class CommonUtilities {
     //public static final String SERVER = "https://www.v3cprojectsdemo.com/deploy/leadryde/";
     //public static final String SERVER = "https://www.leadryde.com/beta/";
     //public static final String SERVER = "https://www.leadryde.com/";
-    public static final String SERVER = "https://app.leadryde.com/";
+   // public static final String SERVER = "https://app.leadryde.com/";
+    public static final String SERVER = "https://demo.leadryde.com/";
     public static final String SERVER_FOLDER_PATH = "";
     //public static final String WEBSERVICE = "webservice_shark.php";
     public static final String WEBSERVICE = "webservice_shark_v2.php";
